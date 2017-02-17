4d7d94946f09ab0457078c16f20969aee506ca5c
Oliver Denman <oliver@therewillbeanimals.com>
2017-02-17 11:14:45 +0000

    Test member row data with area type: circunscripción

--- test/members_page_test.rb
+++ test/members_page_test.rb
@@ -4,14 +4,43 @@ require_relative '../lib/members_page.rb'
 
 describe 'MembersPage' do
   around { |test| VCR.use_cassette('Members Page', &test) }
-
   let :members_page do
     url = 'http://sitl.diputados.gob.mx/LXIII_leg/listado_diputados_gpnp.php?tipot=TOTAL'
     MembersPage.new(response: Scraped::Request.new(url: url).response)
   end
 
-  subject do
-    members_page.party_groupings.first.members.find { |mem| mem.id == id }
+  describe 'member rows' do
+    subject do
+      members_page.party_groupings.flat_map(&:members).find { |mem| mem.id == id }
+    end
+
+    describe 'member row with area type: distrito' do
+      let(:id) { '260' }
+
+      it 'should have the expected data' do
+        subject.to_h.must_equal(
+          id:        id,
+          sort_name: 'Abdala Carmona Yahleel',
+          source:    'http://sitl.diputados.gob.mx/LXIII_leg/curricula.php?dipt=260',
+          area:      'Tamaulipas 1',
+          area_id:   'ocd-division/country:mx/entidad:tamaulipas/distrito:1'
+        )
+      end
+    end
+
+    describe 'member row with area type: circunscripción' do
+      let(:id) { '500' }
+
+      it 'should have the expected data' do
+        subject.to_h.must_equal(
+          id:        id,
+          sort_name: 'Perea Santos Ana Guadalupe',
+          source:    'http://sitl.diputados.gob.mx/LXIII_leg/curricula.php?dipt=500',
+          area:      'Hidalgo 5',
+          area_id:   'ocd-division/country:mx/entidad:hidalgo/circunscripción:5'
+        )
+      end
+    end
   end
 
   describe 'party groupings' do
@@ -29,20 +58,6 @@ describe 'MembersPage' do
 
     it 'should contain a party grouping with the correct number of members' do
       subject.members.count.must_equal 207
-    end
-  end
-
-  describe 'member row' do
-    let(:id) { '260' }
-
-    it 'should have the expected data' do
-      subject.to_h.must_equal(
-        id:        id,
-        sort_name: 'Abdala Carmona Yahleel',
-        source:    'http://sitl.diputados.gob.mx/LXIII_leg/curricula.php?dipt=260',
-        area:      'Tamaulipas 1',
-        area_id:   'ocd-division/country:mx/entidad:tamaulipas/distrito:1'
-      )
     end
   end
 end
